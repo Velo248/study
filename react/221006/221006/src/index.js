a85114032d24a7722798cbdcfcd867c428ab68fd
Velo2 <jhpark4884@gmail.com>
2022-10-07 23:21:59 +0900

    feedback upload

--- react/221006/221006/src/index.js
+++ react/221006/221006/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Feedback from './feedback'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Feedback />
   </React.StrictMode>
 );
 
