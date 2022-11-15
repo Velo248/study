3c3a9746baa55f85fc46269af44c3757ea349010
Velo248 <jhpark4884@gmail.com>
2022-11-15 17:14:47 +0900

    아름 1차 제출

--- js/study.js
+++ js/study.js
@@ -47,7 +47,13 @@ function solution1(dir, degree, matrix) {
   return '';
 }
 
-console.log(solution1(1, 90, [([1, 2, 3], [4, 5, 6], [7, 8, 9])]));
+console.log(
+  solution1(1, 0, [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
 
 /**
  * Q2) 짝이 없는 친구 찾기
